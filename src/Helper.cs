68dd309872a912420975aef39045da7a12c53d5b
Doob <doodledaff@gmail.com>
2025-10-22 20:12:27 -0400

    Switched out image import and deleted another image import not being used

--- src/Helper.cs
+++ src/Helper.cs
@@ -7,106 +7,106 @@ using Godot;
 // should go in dedicated extension method definition file
 public static class Helpers
 {
-    [Flags]
-    public enum Collisions
-    {
-        PLAYER_HIT = 0b0001,
-        PLAYER_HURT = 0b0010,
-        ENEMY_HIT = 0b0100,
-        ENEMY_HURT = 0b1000,
-    }
+	[Flags]
+	public enum Collisions
+	{
+		PLAYER_HIT = 0b0001,
+		PLAYER_HURT = 0b0010,
+		ENEMY_HIT = 0b0100,
+		ENEMY_HURT = 0b1000,
+	}
 
-    public static Vector2 GetMovementVelocity(Vector2 start, Vector2 end, float time)
-    {
-        var dist = new Vector2(end.X - start.X, end.Y - start.Y);
-        GD.Print(dist);
-        return dist / time;
-    }
+	public static Vector2 GetMovementVelocity(Vector2 start, Vector2 end, float time)
+	{
+		var dist = new Vector2(end.X - start.X, end.Y - start.Y);
+		GD.Print(dist);
+		return dist / time;
+	}
 
-    public static void VelocityMovement(
-        this Tween tween,
-        CharacterBody2D node,
-        Vector2 end,
-        float time,
-        bool parallel = true,
-        bool reset = true
-    )
-    {
-        var newTween = node.CreateTween();
-        var start = node.Position;
-        var dist = new Vector2(end.X - start.X, end.Y - start.Y);
-        var prevVel = node.Velocity;
+	public static void VelocityMovement(
+		this Tween tween,
+		CharacterBody2D node,
+		Vector2 end,
+		float time,
+		bool parallel = true,
+		bool reset = true
+	)
+	{
+		var newTween = node.CreateTween();
+		var start = node.Position;
+		var dist = new Vector2(end.X - start.X, end.Y - start.Y);
+		var prevVel = node.Velocity;
 
-        newTween.Call(() => node.Velocity = dist / time);
-        newTween.TweenInterval(time);
-        if (reset)
-        {
-            newTween.Call(() => node.Velocity = prevVel);
-        }
-        else
-        {
-            newTween.Call(() => node.Velocity = Vector2.Zero);
-        }
+		newTween.Call(() => node.Velocity = dist / time);
+		newTween.TweenInterval(time);
+		if (reset)
+		{
+			newTween.Call(() => node.Velocity = prevVel);
+		}
+		else
+		{
+			newTween.Call(() => node.Velocity = Vector2.Zero);
+		}
 
-        if (parallel)
-        {
-            tween.Parallel().TweenSubtween(newTween);
-        }
-        else
-        {
-            tween.TweenSubtween(newTween);
-        }
-    }
+		if (parallel)
+		{
+			tween.Parallel().TweenSubtween(newTween);
+		}
+		else
+		{
+			tween.TweenSubtween(newTween);
+		}
+	}
 
-    public static void Call(this Tween tween, Action action, float delay = 0, bool parallel = false)
-    {
-        if (parallel)
-        {
-            tween.Parallel().TweenCallback(Callable.From(action)).SetDelay(delay);
-        }
-        else
-        {
-            tween.TweenCallback(Callable.From(action)).SetDelay(delay);
-        }
-    }
+	public static void Call(this Tween tween, Action action, float delay = 0, bool parallel = false)
+	{
+		if (parallel)
+		{
+			tween.Parallel().TweenCallback(Callable.From(action)).SetDelay(delay);
+		}
+		else
+		{
+			tween.TweenCallback(Callable.From(action)).SetDelay(delay);
+		}
+	}
 
-    public static Tween MakeMovementTween(
-        this Tween tween,
-        Node2D node,
-        Vector2 newPos,
-        float time,
-        bool parallel = true
-    )
-    {
-        var newTween = node.CreateTween();
-        newTween.TweenProperty(node, "position", newPos, time);
-        if (parallel)
-        {
-            tween.Parallel().TweenSubtween(newTween);
-        }
-        else
-        {
-            tween.TweenSubtween(newTween);
-        }
-        return newTween;
-    }
+	public static Tween MakeMovementTween(
+		this Tween tween,
+		Node2D node,
+		Vector2 newPos,
+		float time,
+		bool parallel = true
+	)
+	{
+		var newTween = node.CreateTween();
+		newTween.TweenProperty(node, "position", newPos, time);
+		if (parallel)
+		{
+			tween.Parallel().TweenSubtween(newTween);
+		}
+		else
+		{
+			tween.TweenSubtween(newTween);
+		}
+		return newTween;
+	}
 
-    public static float FramesToSeconds(int frames)
-    {
-        return (float)frames / 60;
-    }
+	public static float FramesToSeconds(int frames)
+	{
+		return (float)frames / 60;
+	}
 
-    public struct AttackData(int damage, int knockback, int hitstun)
-    {
-        public int damage = damage;
-        public int knockback = knockback;
-        public int hitstun = hitstun;
-    }
+	public struct AttackData(int damage, int knockback, int hitstun)
+	{
+		public int damage = damage;
+		public int knockback = knockback;
+		public int hitstun = hitstun;
+	}
 
-    public static async void Hitstop(float scale, int milliseconds)
-    {
-        Engine.TimeScale = scale;
-        await Task.Delay((int)(milliseconds));
-        Engine.TimeScale = 1;
-    }
+	public static async void Hitstop(float scale, int milliseconds)
+	{
+		Engine.TimeScale = scale;
+		await Task.Delay((int)(milliseconds));
+		Engine.TimeScale = 1;
+	}
 }
